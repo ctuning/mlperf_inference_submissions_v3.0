687a3e90fb8db6365ef2ff6e4c2d9fa1d56660d9
Ubuntu <ubuntu@ip-172-31-9-168.us-west-2.compute.internal>
2023-03-03 16:12:59 +0000

    Added new results

---
+++ tmp-run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export CM_GIT_CHECKOUT_PATH="/home/ubuntu/CM/repos/local/cache/34a44532df0240bf/repo"
+export CM_GIT_REPO_CHECKOUT="main"
+export CM_GIT_REPO_CHECKOUT_FOLDER="repo"
+export CM_GIT_REPO_CHECKOUT_PATH="/home/ubuntu/CM/repos/local/cache/34a44532df0240bf/repo"
+export CM_GIT_REPO_DEPTH="--depth 4"
+export CM_GIT_REPO_NAME="mlperf_inference_submissions_v3.0"
+export CM_GIT_REPO_PATCH="no"
+export CM_GIT_REPO_RECURSE_SUBMODULES=" --recurse-submodules"
+export CM_GIT_REPO_URL="https://github.com/ctuning/mlperf_inference_submissions_v3.0"
+export CM_MLPERF_RESULTS_GIT_REPO_URL="https://github.com/ctuning/mlperf_inference_submissions_v3.0"
+export CM_MLPERF_RESULTS_REPO_COMMIT_MESSAGE="Added new results"
+export CM_MLPERF_SUBMISSION_DIR="/home/ubuntu/pytorch_submisssion"
+export CM_PYTHON_BIN="python3"
+export CM_PYTHON_BIN_PATH="/usr/bin"
+export CM_PYTHON_BIN_WITH_PATH="/usr/bin/python3"
+export CM_PYTHON_CACHE_TAGS="version-3.10.6,non-virtual"
+export CM_PYTHON_VERSION="3.10.6"
+export CM_TMP_CURRENT_PATH="/home/ubuntu/pytorch_submisssion"
+export CM_TMP_CURRENT_SCRIPT_PATH="/home/ubuntu/CM/repos/octoml@ck/cm-mlops/script/push-mlperf-inference-results-to-github"
+export CM_TMP_PIP_VERSION_STRING=""
+
+
+. /home/ubuntu/CM/repos/octoml@ck/cm-mlops/script/push-mlperf-inference-results-to-github/run.sh
